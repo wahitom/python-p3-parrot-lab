27c293281fa85196fea5f303e3c745db8bb5c040
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-27 20:54:45 +0300

    Completed

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,25 @@
-def parrot():
-    pass
+def parrot(arg=None):
+    if arg != None:
+        print(arg)
+        return arg
+    else:
+        print("Squawk!")
+        return "Squawk!"
+    
+# parrot('hello')
+# parrot() 
+
+# def parrot(arg):
+#     print(arg)
+#     return(arg)
+# parrot('hello')
+
+# def print_squawk():
+#     result = parrot()
+#     if(result):
+#         print("Squawk")
+
+# print_squawk()
+
+
+
